9eec3767f8c2f0c491642d2195eb031799055b5b
Tolga Kurt <oycioz@outlook.com>
2024-08-03 11:32:56 +0300

    Agac İlerleme ve Düzenleme

--- Agac.c
+++ Agac.c
@@ -27,57 +27,76 @@ int main(){
 
 	srand(time(NULL));
 
-
 	int noktasayisi=2; // win + lose ile beraber minimum 3 olmali
 	printf("win icin -2 isimli nokta son sirada,lose için -1 isimli nokta sondan bir önceki noktadir.\n");
-	printf("Nokta sayisi giriniz:");
+	printf("(kod girmek için 0 olmak üzere)Nokta sayisi giriniz:"); // !!!!!!!! 0 girilince kod üzerinden aðacý oluþturan programý yaz.. !!!!!!!!!!!!!
 	scanf("%d",&noktasayisi);
+	
+	
+	if(noktasayisi=0){ // DAHA HAZIR DEÐÝL BELÝRTECÝ
+		printf("WORK ÝN PROGRESS");
+	}
+	
 	
 	if(noktasayisi<3){
 		printf("Anlamsiz!!");
 		exit(-1);
 	}
+	
 	if(noktasayisi>15){//MAX NOKTA SAYÝSÝ
 		printf("YAPMA!!");
 		exit(-1);
 	}
+	
 	Nokta liste[noktasayisi];
+	int kod[noktasayisi*2-3];
+	kod[noktasayisi*2-4]=-999;
+	
 	for(int i=0;i<noktasayisi-2;i++){
 		liste[i].name=i;
 	}
+	
 	liste[noktasayisi-1].name=99;//win
 	liste[noktasayisi-2].name=-1;//lose
+	
 	for(int i=0;i<noktasayisi;i++){
 		printf("\n%d isimli %d sirali\n",liste[i].name,i);
 	}
+	
 	int cocukn=0;
 	printf("Liste numarasi giriniz! -2 ismi win -1 ismi lose noktasidir\n");
+	
 	for(int i=0;i<noktasayisi-2;i++){
 		printf("%d numarali noktanin ilk cocugu:",liste[i].name);
 		scanf("%d",&cocukn);
+		kod[2*i]=cocukn;
 		liste[i].c1=&(liste[cocukn]);
 		cocukn=0;
 		printf("%d numarali noktanin ikinci cocugu:",liste[i].name);
 		scanf("%d",&cocukn);
+		kod[2*i+1]=cocukn;
 		liste[i].c2=&(liste[cocukn]);
 		cocukn=0;
-	}	
-	printf("\nBitti\n");
-	
-	for(int i=0;i<noktasayisi-2;i++){
-	
-		printf("\n%d -> %d ve %d\n",liste[i].name,liste[i].c1->name,liste[i].c2->name);
 	}
+		
+	printf("\nKod:");
+	
+	for(int i=0;kod[i]!=-999;i++){
+		printf("%d",kod[i]);
+	}
+	
+	printf("\n");
+
 	int wins=0,loses=0;
 	
 	//BULUNMA ORANI ARRAYI YAP. URBO HESAPLA
+	
 	//WÝNRATE KO HESAPLIYOR
 	
-	
-	
-	
+	Nokta* mevcut=&(liste[0]);
+		
 	for(int j=0;j<1000;j++){//KAÇ KERE BAÞTAN GEZSÝN.
-		Nokta* mevcut=&(liste[0]);
+		mevcut=&(liste[0]);
 		for(int i=0;i<noktasayisi*5;i++){//KAÇ NOKTADAN SONRA SALSIN.
 		//	printf("\n Mevcut %d",mevcut->name);
 		
@@ -87,12 +106,12 @@ int main(){
 		
 			//ÝLERLEDÝK WÝN MÝ LOSE MU CHECK
 			if(mevcut->name==99){
-				printf("\nWIN");
+				//printf("\nWIN");
 				wins++;
 				break;
 			}
 			if(mevcut->name==-1){
-				printf("\nLose");
+			//	printf("\nLose");
 				loses++;
 				break;
 			}
@@ -102,8 +121,6 @@ int main(){
 		
 	
 	}
-	
-	
 	
 	printf("%d tekrar %d win ve %d lose, yani %.3f winrate ve %.3f loserate",wins+loses,wins,loses,(float)(wins)/(wins+loses),(float)(loses)/(wins+loses));
 	
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Ugraslar
 Kendi kendime uğraştığım şeyler
 
-para.c açıklaması:{
+Para.c açıklaması:{
       AMAÇ: P MİKTARINDA SAYİ, BİRBİRLERİYLE TOPLAMA ÇIKARMA VEYA KENDİSİ VEYA KENDİSİ*2 OLMAK ÜZERE, SIRALI BİR ŞEKİLDE MAKSİMUM HANGİ SAYIYA KADAR TÜM SAYILARI ELDE EDEBİLİRİZ.
       	// ÖRNEK 2 VE 3 İLE 1 2 3 4 5 6 ELDE EDİLEBİLİR 
       	// 3-2 2 3 2+2 3+2 3+3
       kod içerisinde matematiksel olarak kesin sonuçların yanı sıra deneysel olarak muhtemel bulduğum sonuçlara bağlı olarak aradığımız sonucun daha hızlı bulunmasını dağlamak amacıyla koyduğum limitler vardır.
 }
+
+Agac.c açıklaması:{
+      AMAÇ: Bir ağaç üzerinde birbiri ile bağlı noktalar arasinda rastgele ilerleyerek başarı noktasina ulaşma ihtimali üzerine incelemeler
+	en basit örnek olarak 0<-a->1 Şeklinde bir "ağaç"ta %50 ihtimal 0, %50 ihtimal 1'e gidersiniz. 
+    }
